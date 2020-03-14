330f81a4711400a7a77602eb2016499967450acd
icruz-dev <icruz@springshare.com>
2020-03-14 19:06:34 -0400

    feat(main): test html modules

--- loader.js
+++ loader.js
@@ -1,3 +1,5 @@
 import PopUpInfo from './components/popup-info.mjs';
+import * as materialLite from './material-lite.html';
 
 customElements.define('popup-info', PopUpInfo);
+document.body.appendChild(materialLite);
